60d40878c9a09f23ca109a31417206c250f7bab1
Tống Phan Kim Thạch <22637901.thach@student.iuh.edu.vn>
2025-08-05 15:57:52 +0700

    update layout new

---
+++ src/Components/RegisterHeader/RegisterHeader.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function RegisterHeader() {
+  return (
+    <header className='py-5'>
+        <div className="max-w-7xl mx-auto px-4">
+            <nav className="flex items-end">
+                
+            </nav>
+        </div>
+    </header>
+
+    
+  )
+}
--- src/layout/RegisterLayout/RegisterLayout.tsx
+++ src/layout/RegisterLayout/RegisterLayout.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import RegisterHeader from '../../Components/RegisterHeader'
+import Footer from '../../Components/Footer'
 interface Props {
   children ?: React.ReactNode
 }
 export default function RegisterLayout({children} : Props) {
   return (
-    <div>RegisterLayout{children}</div>
+    <div>
+      <RegisterHeader/>
+      RegisterLayout{children}
+      <Footer/>
+      </div>
   )
 }
